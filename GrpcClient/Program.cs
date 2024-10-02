546ee16d4bea0089c5650f84106579d7580ba1ee
Arafat Zaman Anik <arafat.zaman.anik@gmail.com>
2024-10-02 06:59:37 +0600

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,8 @@
+﻿using Grpc.Net.Client;
+using GrpcGreetingService; // This should match the namespace in greet.proto
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Arafat" });
+
+Console.WriteLine($"Greeting: {reply.Message}");
